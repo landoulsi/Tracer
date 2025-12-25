f18c405ca961ac8fdc826f1b590fdc63070973d0
Ahmed Landoulsi <landoulsi.ahmed@gmail.com>
2025-12-25 16:15:13 +0400

    Add unit tests for SDK

--- android/sdk/build.gradle.kts
+++ android/sdk/build.gradle.kts
@@ -24,6 +24,10 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.google.code.gson:gson:2.10.1")
+    
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.8.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
 }
 
 afterEvaluate {
